c9df7a83d5951862eea2e535db0185e0fa0b1079
unknown <hussienhamdan06@gmail.com>
2024-02-27 23:16:08 +0200

    my project

---
+++ user_adminlogin/user_adminlogin/Controllers/AdminPackageController.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using user_adminlogin.Data;
+using user_adminlogin.Models;
+using System;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace user_adminlogin.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminPackageController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdminPackageController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var packages = await _context.Packages.FromSqlRaw("EXEC GetPackages").ToListAsync();
+            return View(packages);
+        }
+
+        [HttpGet]
+        public IActionResult AddPackage()
+        {
+            ViewData["Flights"] = new SelectList(_context.Flights.Select(f => new { Id = f.Id, Name = f.flight_Name }).ToList(), "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddPackage(Package package)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Database.ExecuteSqlInterpolated($"EXEC AddPackage {package.Package_details}, {package.Price}, {package.FlightId}");
+
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex)
+                {
+                    // Log the exception or handle it appropriately
+                    // For now, let's return an error view with the exception details
+                    return RedirectToAction("Index");
+                }
+            }
+
+            // If ModelState is not valid, return back to the form with validation errors
+            ViewData["Flights"] = new SelectList(_context.Flights.Select(f => new { Id = f.Id, Name = f.flight_Name }).ToList(), "Id", "Name");
+            return View(package);
+        }
+
+        [HttpGet]
+        public IActionResult UpdatePackage(int id)
+        {
+            // Use AsEnumerable to execute the raw SQL query and materialize the results
+            var package = _context.Packages.FromSqlRaw($"EXEC GetPackageById {id}").AsEnumerable().FirstOrDefault();
+
+            if (package == null)
+            {
+                return NotFound();
+            }
+
+            ViewData["Flights"] = new SelectList(_context.Flights.Select(f => new { Id = f.Id, Name = f.flight_Name }).ToList(), "Id", "Name");
+            return View(package);
+        }
+
+        [HttpPost]
+        public IActionResult UpdatePackage(Package package)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Database.ExecuteSqlInterpolated($"EXEC UpdatePackage {package.Id}, {package.Package_details}, {package.Price}, {package.FlightId}");
+
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex)
+                {
+                    // Log the exception or handle it appropriately
+                    // For now, let's return an error view with the exception details
+                    return RedirectToAction("Index");
+                }
+            }
+
+            // If ModelState is not valid, return back to the form with validation errors
+            ViewData["Flights"] = new SelectList(_context.Flights.Select(f => new { Id = f.Id, Name = f.flight_Name }).ToList(), "Id", "Name");
+            return View(package);
+        }
+
+
+        [HttpGet]
+        public IActionResult DeletePackage(int id)
+        {
+            // Materialize the results using AsEnumerable()
+            var package = _context.Packages.FromSqlRaw($"EXEC GetPackageById {id}").AsEnumerable().FirstOrDefault();
+
+            if (package == null)
+            {
+                return NotFound();
+            }
+
+            return View(package);
+        }
+
+        [HttpPost, ActionName("DeletePackage")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePackageConfirmed(int id)
+        {
+            try
+            {
+                // No need to fetch the package before deleting
+                _context.Database.ExecuteSqlInterpolated($"EXEC DeletePackage {id}");
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                // Log the exception or handle it appropriately
+                // For now, let's return an error view with the exception details
+                return RedirectToAction("Index");
+            }
+        }
+
+    }
+}
---
+++ user_adminlogin/user_adminlogin/Controllers/BooktripController.cs
@@ -0,0 +1,87 @@
+﻿
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Net.Security;
+using System.Security.Claims;
+using user_adminlogin.Data;
+using user_adminlogin.Models;
+
+namespace Flight_dbproject.Controllers
+{
+    [Authorize]
+    public class BooktripController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _db;
+
+        public BooktripController(UserManager<ApplicationUser> userManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var uId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var booked = from b in _db.Bookings
+                        where b.UserId == uId
+                        select b.Flight;
+            var flight = from row in _db.Flights
+                           select row;
+            IEnumerable<Flight> flightList = flight.Except(booked).ToList();
+            return View(flightList);
+        }
+        public async Task<IActionResult> bookNow(int id)
+        {
+            var uId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var exist = from b in _db.Bookings
+                        where b.FlightId == id && b.UserId == uId
+                        select b;
+            if (exist.Any())
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ApplicationUser user = await _userManager.FindByIdAsync(uId);
+                /* var flight = from f in _db.Flights
+                                 where f.Id == flightId
+                                 select f;*/
+                var flight = _db.Flights.Find(id);
+                var booking = new Booking
+                {
+                    UserId = uId,
+                    User = user,
+                    FlightId = id,
+                    Flight = flight
+                };
+                _db.Bookings.Add(booking);
+                _db.SaveChanges();
+                return RedirectToAction("bookingList");
+            }
+        }
+        public ActionResult bookingList()
+        {
+            
+           var id = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var bookings = from row in _db.Bookings
+                           where row.UserId == id
+                           select row.Flight;
+            List<Flight> bookingList = bookings.ToList();
+            return View(bookingList);
+        }
+        public ActionResult cancelFlight(int id)
+        {
+            var uId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var booking = _db.Bookings.FirstOrDefault(b => b.UserId == uId  && b.FlightId == id);
+            Console.WriteLine("********" + booking + "********");
+            if (booking != null)
+            {
+                _db.Bookings.Remove(booking);
+                _db.SaveChanges();
+            }   
+              return RedirectToAction("bookingList");
+        }
+    }
+}
